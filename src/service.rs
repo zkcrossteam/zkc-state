7782934e1d47c93dd16eea8d468487c23feb0d46
John Doe <john@zklab.dev>
2023-07-10 11:44:50 +0800

    wrap mongodb *_with_session operations

--- src/service.rs
+++ src/service.rs
@@ -1,8 +1,15 @@
+use std::borrow::Borrow;
+
 use crate::kvpair::LeafData;
 use crate::Error;
 
 use super::kvpair::{bytes_to_bson, ContractId, Hash, MerkleRecord};
-use mongodb::bson::doc;
+use mongodb::bson::{doc, Document};
+use mongodb::error::{TRANSIENT_TRANSACTION_ERROR, UNKNOWN_TRANSACTION_COMMIT_RESULT};
+use mongodb::options::{
+    Acknowledgment, FindOneOptions, InsertOneOptions, ReadConcern, TransactionOptions, WriteConcern,
+};
+use mongodb::results::InsertOneResult;
 use mongodb::{Client, ClientSession, Collection};
 use tonic::{Request, Response, Status};
 
@@ -14,7 +21,6 @@ pub struct MongoKvPair {
 }
 
 pub struct MongoCollection<T> {
-    client: Client,
     collection: Collection<T>,
     session: Option<ClientSession>,
 }
@@ -32,9 +38,15 @@ impl<T> MongoCollection<T> {
         client: Client,
         contract_id: &ContractId,
         with_session: bool,
-    ) -> Result<Self, Error> {
+    ) -> Result<Self, mongodb::error::Error> {
         let session = if with_session {
-            Some(client.start_session(None).await?)
+            let mut session = client.start_session(None).await?;
+            let options = TransactionOptions::builder()
+                .read_concern(ReadConcern::majority())
+                .write_concern(WriteConcern::builder().w(Acknowledgment::Majority).build())
+                .build();
+            session.start_transaction(options).await?;
+            Some(session)
         } else {
             None
         };
@@ -42,13 +54,35 @@ impl<T> MongoCollection<T> {
         let collection_name = Self::get_collection_name(contract_id);
         let collection = database.collection::<T>(collection_name.as_str());
         Ok(Self {
-            client,
             collection,
             session,
         })
     }
 
-    pub async fn drop(&self) -> Result<(), Error> {
+    pub async fn commit(&mut self) -> Result<(), mongodb::error::Error> {
+        if let Some(session) = self.session.as_mut() {
+            // A "TransientTransactionError" label indicates that the entire transaction can be retried
+            // with a reasonable expectation that it will succeed.
+            // An "UnknownTransactionCommitResult" label indicates that it is unknown whether the
+            // commit has satisfied the write concern associated with the transaction. If an error
+            // with this label is returned, it is safe to retry the commit until the write concern is
+            // satisfied or an error without the label is returned.
+            loop {
+                let result = session.commit_transaction().await;
+                if let Err(ref error) = result {
+                    if error.contains_label(UNKNOWN_TRANSACTION_COMMIT_RESULT)
+                        || error.contains_label(TRANSIENT_TRANSACTION_ERROR)
+                    {
+                        continue;
+                    }
+                }
+                result?
+            }
+        }
+        Ok(())
+    }
+
+    pub async fn drop(&self) -> Result<(), mongodb::error::Error> {
         let options = mongodb::options::DropCollectionOptions::builder().build();
         self.collection.drop(options).await?;
         Ok(())
@@ -56,20 +90,52 @@ impl<T> MongoCollection<T> {
 }
 
 impl MongoCollection<MerkleRecord> {
+    pub async fn find_one(
+        &mut self,
+        filter: impl Into<Option<Document>>,
+        options: impl Into<Option<FindOneOptions>>,
+    ) -> Result<Option<MerkleRecord>, mongodb::error::Error> {
+        let result = match self.session.as_mut() {
+            Some(session) => {
+                self.collection
+                    .find_one_with_session(filter, options, session)
+                    .await?
+            }
+            _ => self.collection.find_one(filter, options).await?,
+        };
+        Ok(result)
+    }
+
+    pub async fn insert_one(
+        &mut self,
+        doc: impl Borrow<MerkleRecord>,
+        options: impl Into<Option<InsertOneOptions>>,
+    ) -> Result<InsertOneResult, mongodb::error::Error> {
+        let result = match self.session.as_mut() {
+            Some(session) => {
+                self.collection
+                    .insert_one_with_session(doc, options, session)
+                    .await?
+            }
+            _ => self.collection.insert_one(doc, options).await?,
+        };
+        Ok(result)
+    }
+
     pub async fn get_merkle_record(
-        &self,
+        &mut self,
         index: u32,
         hash: &Hash,
     ) -> Result<Option<MerkleRecord>, Error> {
         let mut filter = doc! {};
         filter.insert("index", index);
         filter.insert("hash", bytes_to_bson(&hash.0));
-        let record = self.collection.find_one(filter, None).await?;
+        let record = self.find_one(filter, None).await?;
         Ok(record)
     }
 
     pub async fn must_get_merkle_record(
-        &self,
+        &mut self,
         index: u32,
         hash: &Hash,
     ) -> Result<MerkleRecord, Error> {
@@ -77,28 +143,31 @@ impl MongoCollection<MerkleRecord> {
         record.ok_or(Error::Precondition("Merkle record not found".to_string()))
     }
 
-    pub async fn get_root_merkle_record(&self) -> Result<Option<MerkleRecord>, Error> {
+    pub async fn get_root_merkle_record(&mut self) -> Result<Option<MerkleRecord>, Error> {
         let hash = Hash::default();
         let mut filter = doc! {};
         filter.insert("hash", bytes_to_bson(&hash.0));
-        let record = self.collection.find_one(filter, None).await?;
+        let record = self.find_one(filter, None).await?;
         Ok(record)
     }
 
-    pub async fn must_get_root_merkle_record(&self) -> Result<MerkleRecord, Error> {
+    pub async fn must_get_root_merkle_record(&mut self) -> Result<MerkleRecord, Error> {
         let record = self.get_root_merkle_record().await?;
         record.ok_or(Error::Precondition("Merkle record not found".to_string()))
     }
 
     /* We always insert new record as there might be uncommitted update to the merkle tree */
-    pub async fn update_merkle_record(&self, record: &MerkleRecord) -> Result<MerkleRecord, Error> {
+    pub async fn update_merkle_record(
+        &mut self,
+        record: &MerkleRecord,
+    ) -> Result<MerkleRecord, Error> {
         let mut filter = doc! {};
         filter.insert("index", record.index);
         filter.insert("hash", bytes_to_bson(&record.hash));
-        let exists = self.collection.find_one(filter, None).await?;
+        let exists = self.find_one(filter, None).await?;
         exists.map_or(
             {
-                self.collection.insert_one(record, None).await?;
+                self.insert_one(record, None).await?;
                 Ok(*record)
             },
             |record| {
@@ -109,7 +178,7 @@ impl MongoCollection<MerkleRecord> {
     }
 
     pub async fn update_root_merkle_record(
-        &self,
+        &mut self,
         record: &MerkleRecord,
     ) -> Result<MerkleRecord, Error> {
         assert_eq!(record.hash, Hash::default().0);
@@ -161,11 +230,12 @@ impl KvPair for MongoKvPair {
         dbg!(&request);
         let request = request.into_inner();
         let contract_id: ContractId = request.contract_id.as_slice().try_into()?;
-        let collection = self.new_collection(&contract_id, false).await?;
+        let mut collection = self.new_collection(&contract_id, false).await?;
         let index = request.index;
         let hash: Hash = request.hash.as_slice().try_into()?;
         let record = collection.must_get_merkle_record(index, &hash).await?;
         let node = record.try_into()?;
+        collection.commit().await.map_err(Error::from)?;
         dbg!(&record, &node);
         Ok(Response::new(GetLeafResponse {
             node: Some(node),
@@ -181,7 +251,7 @@ impl KvPair for MongoKvPair {
         let request = request.into_inner();
         let contract_id: ContractId = request.contract_id.as_slice().try_into()?;
         // TODO: Should use session here
-        let collection = self.new_collection(&contract_id, false).await?;
+        let mut collection = self.new_collection(&contract_id, false).await?;
         let index = request.index;
         let hash: Hash = request.hash.as_slice().try_into()?;
         let leaf_data: LeafData = request.leaf_data.as_slice().try_into()?;
@@ -191,6 +261,7 @@ impl KvPair for MongoKvPair {
             _ => todo!(),
         };
         let node = record.try_into()?;
+        collection.commit().await.map_err(Error::from)?;
         dbg!(&record, &node);
         Ok(Response::new(GetLeafResponse {
             node: Some(node),
