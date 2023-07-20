604448a43c22295597490972495e86c00914e437
aa <aa@bb.com>
2023-07-20 13:54:46 +0800

    Add test example
    Signed-off-by: Glen <glen@zkcross.org>

---
+++ tests/kvpairhelper.rs
@@ -0,0 +1,8 @@
+
+//implement these apis for kvpair.rs
+//implement these apis by send request to rest server
+
+kvpairhelper::MongoMerkle::construct();
+mt.get_root_hash();
+update_leaf_data_with_proof();
+get_leaf_with_proof();
